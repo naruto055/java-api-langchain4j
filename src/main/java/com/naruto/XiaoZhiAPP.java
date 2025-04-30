d7f4637ef6600d023d11498f65b4ff7d339e1281
Wenqs <1935994680@qq.com>
2025-04-30 09:23:19 +0800

    初始化工程

---
+++ src/main/java/com/naruto/XiaoZhiAPP.java
@@ -0,0 +1,15 @@
+package com.naruto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wenqunsheng
+ * @date 2025/4/30 9:20
+ */
+@SpringBootApplication
+public class XiaoZhiAPP {
+    public static void main(String[] args) {
+        SpringApplication.run(XiaoZhiAPP.class, args);
+    }
+}
