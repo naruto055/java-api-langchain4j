be70dcf804bd2bbada20d294d2d629ba4034f8a8
naruto055 <1935994680@qq.com>
2025-05-07 21:33:44 +0800

    feat：测试聊天记忆的简单实现

--- src/test/java/com/naruto/AIServiceTest.java
+++ src/test/java/com/naruto/AIServiceTest.java
@@ -2,10 +2,15 @@ package com.naruto;
 
 import com.naruto.assistant.Assistant;
 import dev.langchain4j.community.model.dashscope.QwenChatModel;
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.chat.response.ChatResponse;
 import dev.langchain4j.service.AiServices;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
 
 /**
  * @author wenqunsheng
@@ -34,5 +39,33 @@ public class AIServiceTest {
     public void testAssistant() {
         String answer = assistant.chat("你好，你是谁");
         System.out.println(answer);
+    }
+
+    /**
+     * 测试聊天记忆
+     */
+    @Test
+    public void testChatMemory() {
+        String answer1 = assistant.chat("我是张三");
+        System.out.println(answer1);
+
+        String answer2 = assistant.chat("我是谁");
+        System.out.println(answer2);
+    }
+
+    /**
+     * 测试聊天记忆2
+     */
+    @Test
+    public void testChatMemory2() {
+        UserMessage userMessage1 = UserMessage.userMessage("我是张三");
+        ChatResponse chatResponse1 = qwenChatModel.chat(userMessage1);
+        AiMessage aiMessage1 = chatResponse1.aiMessage();
+        System.out.println(aiMessage1.text());
+
+        UserMessage userMessage2 = UserMessage.userMessage("我是谁");
+        ChatResponse chatResponse2 = qwenChatModel.chat(Arrays.asList(userMessage1, aiMessage1, userMessage2));
+        AiMessage aiMessage2 = chatResponse2.aiMessage();
+        System.out.println(aiMessage2.text());
     }
 }
