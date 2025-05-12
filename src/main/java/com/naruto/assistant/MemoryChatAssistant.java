d53921c23e66ad656dcb0e3abbf59cd4b746fd09
Wenqs <1935994680@qq.com>
2025-05-12 19:36:42 +0800

    feat: @V 注解使用，指定参数名称

--- src/main/java/com/naruto/assistant/MemoryChatAssistant.java
+++ src/main/java/com/naruto/assistant/MemoryChatAssistant.java
@@ -2,6 +2,7 @@ package com.naruto.assistant;
 
 
 import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
 import dev.langchain4j.service.spring.AiService;
 
 import static dev.langchain4j.service.spring.AiServiceWiringMode.EXPLICIT;
@@ -19,6 +20,9 @@ import static dev.langchain4j.service.spring.AiServiceWiringMode.EXPLICIT;
 )
 public interface MemoryChatAssistant {
 
-    @UserMessage("你是我的好朋友，请用上海话回答问题，并且添加一些表情符号。{{it}}")
-    String chat(String chatMessage);
+    //@UserMessage("你是我的好朋友，请用上海话回答问题，并且添加一些表情符号。{{it}}")    // 这个it占位符是固定的
+    //String chat(String chatMessage);
+
+    @UserMessage("你是我的好朋友，请用上海话回答问题，并且添加一些表情符号。{{chatMessage}}")
+    String chat(@V("chatMessage") String chatMessage);  // 明确指出占位符的名字就叫chatMessage
 }
