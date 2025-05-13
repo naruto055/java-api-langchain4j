14146585e5ec42f26c080626fe9dfd6f732379ab
Wenqs <1935994680@qq.com>
2025-05-13 09:08:44 +0800

    当大模型调用我们所提供的工具类时，如果有memoryId，会把memoryId也携带进去

--- src/main/java/com/naruto/tools/CalculatorTools.java
+++ src/main/java/com/naruto/tools/CalculatorTools.java
@@ -2,6 +2,7 @@ package com.naruto.tools;
 
 import dev.langchain4j.agent.tool.P;
 import dev.langchain4j.agent.tool.Tool;
+import dev.langchain4j.service.MemoryId;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -12,15 +13,15 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class CalculatorTools {
-    @Tool(name = "加法运算", value = "将两个参数a和b相加并返回运算结果")
-    double sum(@P("加数1") double a, @P("加数2") double b) {
-        log.info("调用加法运算");
+    @Tool
+    double sum(@MemoryId int memoryId, @P("加数1") double a, @P("加数2") double b) {
+        log.info("调用加法运算，memoryId：{}", memoryId);
         return a + b;
     }
 
-    @Tool(name = "平方根运算", value = "计算给定参数的平方根并返回结果")
-    double squareRoot(double x) {
-        log.info("调用平方根运算");
+    @Tool
+    double squareRoot(@MemoryId int memoryId, double x) {
+        log.info("调用平方根运算，memoryId：{}", memoryId);
         return Math.sqrt(x);
     }
 }
